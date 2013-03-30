9957c8ec1b9e1b1327de90fd8b1cacfa9c1404b3
Sam Young <samkityoung@gmail.com>
2013-03-30 15:57:49 -0400

    Fixed registration.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
   	@user = User.new(params[:user])
 
   	if @user.save
-  		flash[:success] = "You are registered!"
+      session[:user_id] = @user.id
+  		flash[:success] = "You are registered, and logged in!"
   		redirect_to root_path
   	else
   		@user.errors.delete(:password_digest)
